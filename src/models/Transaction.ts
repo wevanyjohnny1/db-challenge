eeb2c6ef20efbe134359824a92ccd171185ed547
Johnny <johnnywevany@outlook.com>
2020-05-05 13:51:26 -0300

    Adding cors

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -23,12 +23,12 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @Column()
-  category_id: string;
-
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
+
+  @Column()
+  category_id: string;
 
   @CreateDateColumn()
   created_at: Date;
